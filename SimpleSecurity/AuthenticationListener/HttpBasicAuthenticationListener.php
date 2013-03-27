9ca0300e4bbccae05890cbac39f7822b2e6ebc70
Joseph Rouff <rouffj@gmail.com>
2013-03-27 11:54:47 +0100

    Integrate ArrayAuthenticationProvider in SimpleSecurity
    ArrayAuthenticationProvider is used in HttpBasicAuthenticationListener

--- SimpleSecurity/AuthenticationListener/HttpBasicAuthenticationListener.php
+++ SimpleSecurity/AuthenticationListener/HttpBasicAuthenticationListener.php
@@ -3,12 +3,20 @@
 namespace Rouffj\Bundle\HowtoSecurityBundle\SimpleSecurity\AuthenticationListener;
 
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Rouffj\Bundle\HowtoSecurityBundle\SimpleSecurity\Token\LoginPasswordToken;
 
 class HttpBasicAuthenticationListener implements ListenerInterface
 {
+    private $authenticationProvider;
+
+    public function __construct(AuthenticationProviderInterface $authenticationProvider)
+    {
+        $this->authenticationProvider = $authenticationProvider;
+    }
+
     public function handle(GetResponseEvent $event)
     {
         $request = $event->getRequest();
@@ -20,5 +28,6 @@ class HttpBasicAuthenticationListener implements ListenerInterface
 
         // We retrieve info required to authenticate current user from request and encapsulate them into a Token.
         $token = new LoginPasswordToken($request->headers->get('PHP_AUTH_USER'), $request->headers->get('PHP_AUTH_PWD'));
+        $authenticatedToken = $this->authenticationProvider->authenticate($token);
     }
 }
