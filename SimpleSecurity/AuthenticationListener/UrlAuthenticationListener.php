e0b123987442553e983edce42550b94a83f2d8a0
Joseph Rouff <rouffj@gmail.com>
2013-03-29 17:52:01 +0100

    Create an UrlAuthenticationListener

---
+++ SimpleSecurity/AuthenticationListener/UrlAuthenticationListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Rouffj\Bundle\HowtoSecurityBundle\SimpleSecurity\AuthenticationListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\Security\Core\Authentication\Provider\AuthenticationProviderInterface;
+use Symfony\Component\Security\Http\Firewall\ListenerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Rouffj\Bundle\HowtoSecurityBundle\SimpleSecurity\Token\LoginPasswordToken;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+use Rouffj\Bundle\HowtoSecurityBundle\SimpleSecurity\AuthenticationEntryPoint\HttpBasicAuthenticationEntryPoint;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+
+class UrlAuthenticationListener implements ListenerInterface
+{
+    private $authenticationProvider;
+    private $securityContext;
+    private $authenticationEntryPoint;
+
+    public function __construct(AuthenticationProviderInterface $authenticationProvider, SecurityContextInterface $securityContext)
+    {
+        $this->authenticationProvider = $authenticationProvider;
+        $this->securityContext = $securityContext;
+        $this->authenticationEntryPoint = new HttpBasicAuthenticationEntryPoint();
+    }
+
+    public function handle(GetResponseEvent $event)
+    {
+        // Here should be the code to retrieve the user info useful for authenticate him.
+    }
+}
